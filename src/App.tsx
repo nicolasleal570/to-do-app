bc40839f4b548fe9148453b2afeedaa416ed5eaa
Nicolas Leal <lealnicolas96@gmail.com>
2021-05-27 21:34:30 -0400

    Adding prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import Routes from "./routes/Routes"
+import Routes from './routes/Routes';
 
 function App() {
   return (
     <div className="App">
-        <Routes />
+      <Routes />
     </div>
   );
 }
